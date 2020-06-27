5a1fbd2779090a437348c08ca85b7f0336ba93ff
MayYuNwe <yayu79923@gmail.com>
2020-06-27 17:49:02 +0630

    All

---
+++ itProject/src/main/java/com/example/itproject/entity/Java.java
@@ -0,0 +1,86 @@
+package com.example.itproject.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="java")
+public class Java {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="lnforLearn")
+	private String lnforLearn;
+	
+	@Column(name="lnforPj")
+	private String lnforPj;
+	
+	@Column(name="lnforUtube")
+	private String lnforUtube;
+	
+	@Column(name="lnforFb")
+	private String lnforFb;
+
+	public Java(String lnforLearn, String lnforPj, String lnforUtube, String lnforFb) {
+		super();
+		this.lnforLearn = lnforLearn;
+		this.lnforPj = lnforPj;
+		this.lnforUtube = lnforUtube;
+		this.lnforFb = lnforFb;
+	}
+
+	public Java() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLnforLearn() {
+		return lnforLearn;
+	}
+
+	public void setLnforLearn(String lnforLearn) {
+		this.lnforLearn = lnforLearn;
+	}
+
+	public String getLnforPj() {
+		return lnforPj;
+	}
+
+	public void setLnforPj(String lnforPj) {
+		this.lnforPj = lnforPj;
+	}
+
+	public String getLnforUtube() {
+		return lnforUtube;
+	}
+
+	public void setLnforUtube(String lnforUtube) {
+		this.lnforUtube = lnforUtube;
+	}
+
+	public String getLnforFb() {
+		return lnforFb;
+	}
+
+	public void setLnforFb(String lnforFb) {
+		this.lnforFb = lnforFb;
+	}
+	
+	
+
+	
+
+}
---
+++ itProject/src/test/java/com/example/itproject/ItprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
